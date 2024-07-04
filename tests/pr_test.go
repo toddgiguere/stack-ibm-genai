6fff0116287911a9703083a04c2a43cab84005aa
Daniel Butler <dannyeb@gmail.com>
2024-07-04 12:58:28 +0100

    fix: Enable catalog onboarding (#16)
    * enable catalog onboarding

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testprojects"
 	"testing"
 )
@@ -15,15 +14,10 @@ func TestProjectsFullTest(t *testing.T) {
 		ParallelDeploy: true,
 	})
 
-	privateKey, _, kerr := common.GenerateTempGPGKeyPairBase64()
-	if kerr != nil {
-		t.Fatal(kerr)
-	}
 	options.StackInputs = map[string]interface{}{
 		"resource_group_name":          options.ResourceGroup,
 		"ibmcloud_api_key":             options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"],
 		"prefix":                       options.Prefix,
-		"signing_key":                  privateKey,
 		"secret_manager_service_plan":  "trial",
 		"enable_platform_logs_metrics": false,
 	}
